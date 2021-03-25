9bb432a81d233c50c1588f64eb819505c590d6d4
ChanningKuo <492689363@qq.com>
2021-03-25 21:17:37 +0800

    ...

--- WKWebViewContainer/KWKWebView/SettingsTableViewCell.swift
+++ WKWebViewContainer/KWKWebView/SettingsTableViewCell.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 
+@IBDesignable
 class SettingsTableViewCell: UITableViewCell {
+    
+    @IBInspectable var title: UILabel!
     
     init(style: UITableViewCell.CellStyle, section: Int, row: Int) {
         let reuseIdentifier = ""
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
     }
     
     required init?(coder: NSCoder) {
@@ -22,6 +24,7 @@ class SettingsTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
+        title = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width / 2, height: self.frame.height))
     }
     
     public func setup(_ indexPath: IndexPath) {
